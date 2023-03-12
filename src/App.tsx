0daed1ad63d4b93e603a04757ca36ad39efe51be
Rohit <i.rohit003@gmail.com>
2023-03-12 22:53:28 +0530

    night commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
-// import Message from "./Message";
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
 
   return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
-    </div>
+    <>
+    <Button onClick={()=>console.log('Clicked')}>My Button</Button>
+    </>
   );
 }
 
