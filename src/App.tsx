3f16f9854626f1319118bb372027dc6fd2f843c4
Rohit-gi <i.rohit003@gmail.com>
2023-03-12 19:10:57 +0530

    evening commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import Message from "./Message";
+// import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 
-function App(){
-  return <div><Message/></div>
+function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
