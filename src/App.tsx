ac3ef777add10a8a46cde9cb3e5910648ef1ed30
Rohit-gi <i.rohit003@gmail.com>
2023-03-13 20:15:21 +0530

    evening push

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
-
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
     <>
-    <Button onClick={()=>console.log('Clicked')}>My Button</Button>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
     </>
   );
 }
