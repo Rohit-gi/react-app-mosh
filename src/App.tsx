43b0ad2e8f03d82116fbfe7586cae0640a0c0c64
Rohit-gi <i.rohit003@gmail.com>
2023-03-14 20:19:26 +0530

    night commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,49 @@
 // import { useState } from "react";
 // import Alert from "./components/Alert";
 // import Button from "./components/Button";
-import ListGroup from "./components/ListGroup";
-import './App.css';
+// import ListGroup from "./components/ListGroup";
+// import './App.css';
 
-function App() {
-  const items = ["New York", "Los Angeles", "San Francisco"]
+// function App() {
+//   const items = ["New York", "Los Angeles", "San Francisco"]
 
-  return (
+//   return (
+//     <div>
+//       <ListGroup heading="Miami" items={items} onSelectItem={()=>{}} />
+//     </div>
+//   );
+// }
+
+// import {BsCalendarFill} from 'react-icons/bs';
+
+// function App(){
+
+//   return(
+//     <div>
+//       <BsCalendarFill/>
+//     </div>
+//   );
+// }
+
+// import Button from './components/Button/Button';
+
+// function App(){
+
+//   return(
+//     <div>
+//       <Button onClick= {() => {}}>My Button</Button>
+//     </div>
+//   );
+// }
+// export default App;
+
+import Like from "./components/ListGroup/Like";
+
+function App(){
+
+  return(
     <div>
-      <ListGroup heading="Miami" items={items} onSelectItem={()=>{}} />
+      <Like onCLick={() => console.log('clicked')}/>
     </div>
   );
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,11 +1,19 @@
-import {ReactNode} from "react";
-interface Props{
-    children: ReactNode;
-}
-const Alert = ({children}: Props) => {
-  return (
-    <div className="alert alert-primary">{children}</div>
-  )  
-} 
+import { ReactNode } from "react";
 
-export default Alert
+interface Props {
+  children: ReactNode;
+  onClose: () => void;
+}
+
+const Alert = ({ children, onClose }: Props) => {
+  return (
+    <div
+      className="alert alert-primary alert-dismissible fade show"
+    >
+      {children}
+      <button type="button" className="btn-close" onClick={onClose} data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+  );
+};
+
+export default Alert;
