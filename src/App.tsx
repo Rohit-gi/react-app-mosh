817f6acffe7a825dc172b0d74a715b20256da5de
Rohit <i.rohit003@gmail.com>
2023-03-19 13:17:12 +0530

    1pm commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import { useState } from "react";
+import { useState } from "react";
 // import Alert from "./components/Alert";
 // import Button from "./components/Button";
 // import ListGroup from "./components/ListGroup";
@@ -37,15 +37,40 @@
 // }
 // export default App;
 
-import Like from "./components/ListGroup/Like";
+// import Like from "./components/ListGroup/Like";
+
+// function App(){
+
+//   return(
+//     <div>
+//       <Like onClick={() => console.log('clicked')}/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// function App() {
+//   const [pizza, setPizza] = useState({
+//     name: "Spicy Pepperoni",
+//     toppings: ["Mushroom"],
+//   });
+// }
 
 function App(){
+  const [cart, setCart] = useState({
+    discount: .1;
+    items: [
+      {id:1, title: 'Product 1', quantity: 1}
+      {id:2, title: 'Product 2', quantity: 1}
+    ]
+  }
+});
 
-  return(
-    <div>
-      <Like onClick={() => console.log('clicked')}/>
-    </div>
-  );
+const handleClick = () =>{
+
+setCart ({...cart, items: cart.items.map(item => item.id === 1 ? {...item, quantity: item.quantity + 1 }: item)})
+
 }
 
 export default App;
