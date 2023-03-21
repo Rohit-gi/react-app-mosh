eba6df60479ad1aa84c982ac3ebebebd6b267f56
Rohit <i.rohit003@gmail.com>
2023-03-21 22:09:00 +0530

    pushed

--- src/expense-tracker/ExpenseList.tsx
+++ src/expense-tracker/ExpenseList.tsx
@@ -1,23 +1,43 @@
-import React from 'react'
+import React from "react";
 
-const ExpenseList = () => {
+interface Expense {
+  id: number;
+  description: string;
+  amount: number;
+  category: string;
+}
+interface Props {
+  expenses: Expense[];
+  onDelete: (id: number) => void;
+}
+
+const ExpenseList = ({ expenses, onDelete }: Props) => {
   return (
     <div>
-      <table className='table table-bordered'>
+      <table className="table table-bordered">
         <thead>
-            <tr>
-                <th>Description</th>
-                <th>Amount</th>
-                <th>Category</th>
-                <th></th>
-            </tr>
+          <tr>
+            <th>Description</th>
+            <th>Amount</th>
+            <th>Category</th>
+            <th></th>
+          </tr>
         </thead>
         <tbody>
-
+          {expenses.map((expense) => (
+            <tr key={expense.id}>
+              <td>{expense.description}</td>
+              <td>{expense.amount}</td>
+              <td>{expense.category}</td>
+              <td>
+                <button className="btn btn-outline-danger">Delete</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default ExpenseList
+export default ExpenseList;
