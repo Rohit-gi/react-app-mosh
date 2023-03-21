77cc869a263d87d7b0d01e3212b9d893a5ae1d85
Rohit-gi <i.rohit003@gmail.com>
2023-03-21 20:41:04 +0530

    night commit

---
+++ src/expense-tracker/ExpenseList.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const ExpenseList = () => {
+  return (
+    <div>
+      <table className='table table-bordered'>
+        <thead>
+            <tr>
+                <th>Description</th>
+                <th>Amount</th>
+                <th>Category</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default ExpenseList
