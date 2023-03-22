1f119e646e3588cd91a4ba76d5dcb040db92cb3f
Rohit <i.rohit003@gmail.com>
2023-03-22 21:40:43 +0530

    added categories form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 // import ExpandableText from "./components/ExpandableText";
 // import Alert from "./components/Alert";
 // import Button from "./components/Button";
@@ -84,8 +83,12 @@ import { useState } from "react";
 //     )
 // }
 
+import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+
+export const categories = ["Groceries", "Utilities", "Entertainment"];
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -102,6 +105,9 @@ function App() {
 
   return (
     <div>
+      <div className="mb-5">
+        <ExpenseForm />
+      </div>
       <div className="mb-3">
         <ExpenseFilter
           onSelectCategory={(category) => setSelectedCategory(category)}
--- src/expense-tracker/components/ExpenseFilter.tsx
+++ src/expense-tracker/components/ExpenseFilter.tsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React from "react";
+import { categories } from "../../App";
 
 interface Props {
-    onSelectCategory: (category: string) => void;
+  onSelectCategory: (category: string) => void;
 }
 
 const ExpenseFilter = ({ onSelectCategory }: Props) => {
   return (
     <div>
-      <select className="form-select" onChange={(event) => onSelectCategory(event.target.value)}>
+      <select
+        className="form-select"
+        onChange={(event) => onSelectCategory(event.target.value)}
+      >
         <option value="">All categories</option>
-        <option value="Groceries">Groceries</option>
-        <option value="Utilities">Utilities</option>
-        <option value="Entertainment">Entertainment</option>
+        {categories.map((category) => (
+          <option key={category} value={category}>
+            {category}
+          </option>
+        ))}
       </select>
     </div>
-  )
-}
+  );
+};
 
-export default ExpenseFilter
+export default ExpenseFilter;
