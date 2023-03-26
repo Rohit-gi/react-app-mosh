447507e4c75ff7d3d662dd95f338c5e592d7932e
Rohit-gi <i.rohit003@gmail.com>
2023-03-26 18:59:31 +0530

    added custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -231,40 +231,17 @@
 
 // export default App;
 
-import { useEffect, useState } from "react";
-import { CanceledError } from "./services/api-client";
 import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 
-function App(): JSX.Element {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    const { request, cancel } = userService.getAllUsers();
-    request
-      .then((res) => {
-        setUsers(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-
-    return () => cancel();
-  }, []);
+function App() {
+  const { users, error, isLoading, setUsers, setError } = useUsers();
 
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
-    setUsers(users.filter((u) => u.id !== user.id));
+    setUsers(users.filter((u: any) => u.id !== user.id));
 
-    userService.deleteUser(user.id).catch((err) => {
+    userService.delete(user.id).catch((err) => {
       setError(err.message);
       setUsers(originalUsers);
     });
@@ -274,10 +251,8 @@ function App(): JSX.Element {
     const originalUsers = [...users];
     const newUser = { id: 0, name: "Rohit" };
     setUsers([newUser, ...users]);
-
-    
     userService
-      .createUser(newUser)
+      .create(newUser)
       .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
       .catch((err) => {
         setError(err.message);
@@ -288,9 +263,9 @@ function App(): JSX.Element {
   const updateUser = (user: User) => {
     const originalUsers = [...users];
     const updatedUser = { ...user, name: user.name + "!" };
-    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+    setUsers(users.map((u: any) => (u.id === user.id ? updatedUser : u)));
 
-    userService.updateUser(updatedUser).catch((err) => {
+    userService.update(updatedUser).catch((err) => {
       setError(err.message);
       setUsers(originalUsers);
     });
@@ -305,7 +280,7 @@ function App(): JSX.Element {
       </button>
 
       <ul className="list-group">
-        {users.map((user) => (
+        {users.map((user: any) => (
           <li
             key={user.id}
             className="list-group-item d-flex justify-content-between"
